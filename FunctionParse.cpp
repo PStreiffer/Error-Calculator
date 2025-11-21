a4f8c573a1483f68d9d194d812c9a01e51f4aac0
PStreiffer <spacekid243@gmail.com>
2025-11-21 09:15:35 -0500

    added basic +-*/ and ^ detection functionality

--- FunctionParse.cpp
+++ FunctionParse.cpp
@@ -1,15 +1,30 @@
 #include "ErrorClass.h"
 #include <vector>
 #include <variant>
+#include <iostream>
+#include <string>
 using namespace std;
 
 errval functiondo(errval (*f)(vector<errval>), vector<errval> x){
     return f(x);
 }
 
-errval functionparse(string input){
-    vector<vector<variant<string,int>>> args ={};
-    while(input.find("+")!=-1){
-        args.push_back({input.find("+"),1});
+vector<vector<int>> functionparse(string input){
+    vector<char> arglist = {'+','-','*','/','^'};
+    vector<vector<int>> argpostype = {};
+    for (int i = 0; i<input.length();i++){
+        for(int argnum=0; argnum < arglist.size();argnum++){
+            if(input[i]==arglist[argnum]){
+                argpostype.push_back({i,argnum});
+            }
+        }
+        
     }
+    return argpostype;
+}
+
+int main(){
+    string func = "2 + 2 / 3 * 5 -83";
+    vector<vector<int>> c = functionparse(func);
+    return 0;
 }
