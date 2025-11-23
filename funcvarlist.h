714b749c7e81aa57a261c4720c5b1ea7d55a0076
PStreiffer <spacekid243@gmail.com>
2025-11-23 18:11:45 -0500

    added new base functions

--- funcvarlist.h
+++ funcvarlist.h
@@ -18,4 +18,8 @@ class createdfunc {
     }
 };
 
-vector<createdfunc> funclist = {createdfunc("sqrt",[](vector<errval> a){return sqrt(a[0]);})};
+vector<createdfunc> funclist = {createdfunc("sqrt",[](vector<errval> a){return sqrt(a[0]);}),
+                                createdfunc("sin",[](vector<errval> a){return sin(a[0]);}),
+                                createdfunc("cos",[](vector<errval> a){return cos(a[0]);}),
+                                createdfunc("tan",[](vector<errval> a){return tan(a[0]);})
+                            };
