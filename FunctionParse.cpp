620510de74e046f7d173d379bb38f1d51e1c60bf
PStreiffer <spacekid243@gmail.com>
2025-11-23 18:12:43 -0500

    changed paren parsing to be correct, started alpha

--- FunctionParse.cpp
+++ FunctionParse.cpp
@@ -3,10 +3,13 @@
 #include <variant>
 #include <iostream>
 #include <string>
+#include "funcvarlist.h"
+#include <algorithm>
 using namespace std;
 
 
 vector<vector<double>> functionparse(string input){
+    sort(funclist.begin(),funclist.end(),[](createdfunc x, createdfunc y){return size(x.name)>size(y.name);});
     vector<char> arglist = {'+','-','*','/','^', '(', ')','='};
     vector<vector<double>> argpostype = {};
     //+-*/^ operator recognition & number concat
@@ -16,6 +19,22 @@ vector<vector<double>> functionparse(string input){
                 argpostype.push_back({static_cast<double>(arglist[argnum])});
             }
         }
+        if(input[i] == ')'){ //paren matching
+                    try{
+                    for(int i2 = size(argpostype)-2;i2>=0;i2--){
+                        if(argpostype[i2][0] == '(' && size(argpostype[i2])==1){
+                            argpostype[i2].push_back(size(argpostype)-1);
+                            break;
+                        }
+                        if(i2 == 0){
+                            throw(i);
+                        }
+                    }
+                    } catch (int x){
+                        cout<<"Invalid closed parentheses\n";
+                        return {{560}};
+                    }
+                }
         if(isdigit(input[i])|| input[i]=='.'){ //number concatenation
             string s = "";
             s+=input[i];
@@ -31,41 +50,24 @@ vector<vector<double>> functionparse(string input){
         if(isalpha(input[i])&& input[i]!='E'&&input[i]!='e'){ //alpha concatenation
             string s = "";
             s+=input[i];
-            int n = i+1;
-            while(isalpha(input[n])&& input[n]!='E'&&input[n]!='e'){
+            for(int n = i+1;isalpha(input[n])&& input[n]!='E'&&input[n]!='e';n++){
                 s+=static_cast<char>(input[n]);
-                n++;
-            } if (input[n]=='('){
             }
-            i = n-1;
-            
-            argpostype.push_back({0,});
-        }
-    }
-    int parennum  = 0;
-    try{
-    for (int i = 0; i<size(argpostype);i++){ //parentheses validation
-        int closednum = 0;
-        if (argpostype[i][0] == '('){
-            parennum+=1;
-            for(int i1 = size(argpostype)-1; i1>=0;i1--){
-                if(argpostype[i1][0]== ')'){
-                    closednum+=1;
-                    if(closednum==parennum){
-                    argpostype[i].push_back(i1);
-                    break;
-                    }
+            if(input[i+size(s)]=='('){
+            for(int funcnum = 0; funcnum <size(funclist);funcnum++){
+                if(s.find(funclist[funcnum].name)==size(s)-size(funclist[funcnum].name) && size(funclist[funcnum].name)<=size(s)){
+                    s.erase(size(s)-size(funclist[funcnum].name),size(s)-1);
+                    argpostype.push_back({-1,static_cast<double>(funcnum)});
                 }
             }
-            if(parennum!=closednum){
-                    throw 1;
-                    break;
-                }
+            }
+            i+=s.size()-1;
         }
-    
     }
-    } catch (int s){
-        cout<<"Mismatched Parentheses";
+    for(vector<double>arg: argpostype){
+        if(arg[0]=='('&&size(arg)==1){
+            cout<<"Invalid open parentheses\n";
+        }
     }
     
     
@@ -73,7 +75,8 @@ vector<vector<double>> functionparse(string input){
 }
 
 int main(){
-    string func = "(3+5-(85.2*(9*5))-2E8)";
+    string func = "(3+5-(cos(85.2)*(9*5))-2E8)";
     vector<vector<double>> c = functionparse(func);
+    vector<createdfunc> d = funclist;
     return 0;
 }
