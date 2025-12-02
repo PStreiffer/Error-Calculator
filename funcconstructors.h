a478fc276ecce6c680ba51adcdc2bcbc6c8ca5ac
PStreiffer <spacekid243@gmail.com>
2025-12-01 22:06:24 -0500

    changed assigner function to return void, changes function itself

--- funcconstructors.h
+++ funcconstructors.h
@@ -30,7 +30,7 @@ createdfunc concat(const createdfunc upperfunc, const vector<createdfunc> argfun
     );
 };
 
-createdfunc assigner(const createdfunc func, vector<errval> constants, vector<int> assigntype){
+void assigner(createdfunc func, vector<errval> constants, vector<int> assigntype){
     /*assigntype dictates what each variable is:
         - <0 - constant: does not take in value, constant number equal to number in assigntype plus one
         - >=0 - variable: takes in value, variable number equal to number in assigntype
@@ -42,7 +42,7 @@ createdfunc assigner(const createdfunc func, vector<errval> constants, vector<in
             totalargs = type;
         }
     }
-    return createdfunc(
+    func = createdfunc(
         func.name, //name
         totalargs+1, //argnum
         [constants,assigntype,func](vector<errval> a){
@@ -59,3 +59,13 @@ createdfunc assigner(const createdfunc func, vector<errval> constants, vector<in
     );
     
 };
+/*test code
+int main(){
+    auto c = concat(addition, {multiplication, division});
+    cout<<c({1,2,3,4})<<"\n";
+    auto d = concat(division,{multiplication,division});
+    cout<<d({1,2,3,4})<<"\n";
+    cout<<c({1,2,3,4})<<"\n";
+    return 0;
+}
+*/
